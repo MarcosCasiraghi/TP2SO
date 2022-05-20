d57b4ea9c6463181cdc6a802d140b00f04ae7235
tgaybare <tgaybare@itba.edu.ar>
2022-05-20 14:59:06 -0300

    Added interruptions

--- x64barebones/Kernel/irqDispatcher.c
+++ x64barebones/Kernel/irqDispatcher.c
@@ -3,17 +3,25 @@
 #include <naiveConsole.h>
 
 static void int_20();
-static void int_60();
+static void int_80();
+static void int_81();
+static void int_82();
 
 void irqDispatcher(uint64_t irq) {
 	switch (irq) {
 		case 0:
 			int_20();
 			break;
+        case 1:
+            int_80();
+            break;
 		case 2:
-			int_60();
+			int_81();
 			break;
-	}
+        case 3:
+            int_82();
+            break;
+    }
 	return;
 }
 
@@ -21,6 +29,15 @@ void int_20() {
 	timer_handler();
 }
 
-void int_60(){
+void int_80(){
 	ncPrint("hola mundo");
 }
+
+void int_81(){
+
+}
+
+void int_82(){
+    ncClear();
+}
+
