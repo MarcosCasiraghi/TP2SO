ec8da445f882e8cc891266ce971537b7e2eedb88
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2022-05-24 18:52:37 -0300

    Agregado de shell

---
+++ x64barebones/Userland/SampleCodeModule/shell.c
@@ -0,0 +1,35 @@
+#include <shell.h>
+
+typedef void (*functionPointer)(void);
+
+typedef struct{
+    char * name;
+    functionPointer func;
+}FunctionType;
+
+
+static FunctionType programs[] = {{"fibonacci", &fibonacci}, {0,0}};
+
+int run(char * buffer){
+    for( int i = 0 ; programs[i].name ; i++){
+        if( strcmp(buffer, programs[i].name) == 0){
+            programs[i].func();
+            return 0;
+        }
+    }
+    print(buffer, RED, BLACK);
+    print(" is not a valid command", WHITE, BLACK);
+    return 1;
+}
+
+int strcmp(char * str1, char * str2){
+    int i = 0 ;
+    for( ; str1[i] && str2[i]; i++){
+         if(str1[i] != str2[i])
+            return 1;   
+    }
+    if( str1[i] || str2[i] )
+        return 2;
+    return 0;
+}
+
