5b09ca15e14a482e739d3142907a3fd9bdcd5ef2
tgaybare <tgaybare@itba.edu.ar>
2022-05-28 18:34:18 -0300

    Funcionalidades del Scheduler

--- x64barebones/Kernel/scheduler.c
+++ x64barebones/Kernel/scheduler.c
@@ -1,9 +1,14 @@
 #define MAX_TASKS 2
+#define READY 0
+#define HALTED 1
+#define KILLED 2
 typedef void (*functionPointer)(void);
 typedef struct{
     char * name;
     functionPointer func;
     int present;
+    int status;
+    int pID;
 }FunctionType;
 static int hasTasks =0;
 static FunctionType tasks[MAX_TASKS];
@@ -15,18 +20,37 @@ void add_task(char *name, void * task){
             tasks[i].func=task;
             tasks[i].name=name;
             tasks[i].present = 1;
+            tasks[i].status = READY;
+            tasks[i].pID = i;
             return;
         }
         i++;
     }
     //si llega aca ya tengo dos tasks
 }
+
+int haltProcess(int pID){
+    if (tasks[pID].present) {
+        tasks[pID].status = HALTED;
+        return 1;
+    }
+    return -1;
+}
+
+int killProcess(int pID){
+    if (tasks[pID].present) {
+        tasks[pID].status = KILLED;
+        return 1;
+    }
+    return -1;
+}
+
 void scheduler(void){
 
     void (*p)(void);
     while(1){
         for(int i=0;i<MAX_TASKS;i++){
-            if(tasks[i].present==1){
+            if(tasks[i].present && tasks[i].status == READY){
                 p=(void*)tasks[i].func;
                 (*p)();
             }
