1b2f9755b210839b8d7e3473b31e597b680596a7
Tomas Camilo Gay Bare <tomigayba02yt@gmail.com>
2022-09-22 23:36:24 -0300

    Memory manager thought process

--- x64barebones/Kernel/MemoryManager.c
+++ x64barebones/Kernel/MemoryManager.c
@@ -8,9 +8,26 @@ typedef struct MemoryManagerCDT {
     char *nextAddress;
 } MemoryManagerCDT;
 
+typedef struct memoryAllocations {
+    pid_t pId;
+    void *const restrict memStart;
+    int memQty;
+} memoryAllocations;
+
+typedef struct Node{
+    memoryAllocations current;
+    struct Node * next;
+}Node;
+
+static Node * first;
+
+static size_t usedMemory = 0;
+
 MemoryManagerADT createMemoryManager(void *const restrict memoryForMemoryManager, void *const restrict managedMemory) {
     MemoryManagerADT memoryManager = (MemoryManagerADT) memoryForMemoryManager;
     memoryManager->nextAddress = managedMemory;
+
+    first = malloc(sizeof(Node));
 
     return memoryManager;
 }
@@ -20,5 +37,29 @@ void *allocMemory(MemoryManagerADT const restrict memoryManager, const size_t me
 
     memoryManager->nextAddress += memoryToAllocate;
 
-    return (void *) allocation;
+    //chequeo de que next no sea null
+
+//    if (next.memStart - (current.memStart + current.memQty) >= memoryToAllocate) {
+//        usedMemory += memoryToAllocate;
+//        return (void *) allocation;
+//    }
+//
+//    int found = 0;
+//    int notAvail = 0;
+//
+//    While(found == 0 && notAvail == 0){
+//        //recursivo pa encontrar lugar pal proceso
+//        //encuentra -> found = 1
+//        //no entra -> notAvail = 1
+//    }
+//
+//
+//    if (found == 1){
+//        //creacion de lista y suma de usedMemory
+//    }
+//    else if (notAvail == 1){
+//        //nada
+//    }
+
+
 }
