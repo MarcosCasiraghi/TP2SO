6c8d0c5d7de5b06befaecdb7601c884fa9e76260
TobiasPerry <tperry@itba.edu.ar>
2022-11-01 20:39:51 -0300

    test sync (commented)

---
+++ x64barebones/Userland/SampleCodeModule/include/test_sync.h
@@ -0,0 +1,14 @@
+// #ifndef _SYNCTEST_H
+// #define _SYNCTEST_H
+
+// #include <stdint.h>
+// #include <utilities.h>
+// #include <syscalls.h>
+// #include <util.h>
+// #include <string.h>
+
+// uint64_t test_sync(uint64_t argc, char *argv[]);
+// uint64_t my_process_inc(uint64_t argc, char *argv[]);
+// void slowInc(int64_t *p, int64_t inc);
+
+// #endif
